4bfe97a42a81bcbd596418f3244dbddbe8b80e1d
xhn7gjc5xn-maker <xhn7gjc5xn@privaterelay.appleid.com>
2026-03-04 18:47:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let products = {
-  parfum1: { volume: 50, price: 79 },
-  parfum2: { volume: 50, price: 69 }
-};
+let volume = 50;
+let price = 79;
 
-let selectedProduct = "";
-
-function selectVolume(product, vol, pr) {
-  products[product].volume = vol;
-  products[product].price = pr;
-  document.getElementById("selectedVolume").innerText = vol;
-  document.getElementById("selectedPrice").innerText = pr;
+function selectVolume(v, p) {
+  volume = v;
+  price = p;
+  document.getElementById("selectedVolume").innerText = v;
+  document.getElementById("selectedPrice").innerText = p;
 }
 
-function openModal(product) {
-  selectedProduct = product;
+function openModal() {
   document.getElementById("modal").style.display = "block";
-  document.getElementById("selectedVolume").innerText = products[product].volume;
-  document.getElementById("selectedPrice").innerText = products[product].price;
+  document.getElementById("selectedVolume").innerText = volume;
+  document.getElementById("selectedPrice").innerText = price;
 }
 
 function closeModal() {
@@ -27,7 +22,7 @@ function sendOrder() {
   let name = document.getElementById("name").value;
   let contact = document.getElementById("contact").value;
 
-  let message = `Заказ ${selectedProduct}\nОбъем: ${products[selectedProduct].volume} мл\nЦена: €${products[selectedProduct].price}\nИмя: ${name}\nКонтакт: ${contact}`;
+  let message = `Заказ Parfum1\nОбъем: ${volume} мл\nЦена: €${price}\nИмя: ${name}\nКонтакт: ${contact}`;
   let url = `https://t.me/YOUR_USERNAME?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 }
